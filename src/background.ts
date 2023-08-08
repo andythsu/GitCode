b05aecc0fff4be4bc3ee5bbb3476ae718d050ae4
Andy <f2280c@gmail.com>
2023-08-08 00:12:05 -0400

    init lcProfile inside self-starting script

--- src/background.ts
+++ src/background.ts
@@ -1,29 +1,32 @@
 import { LCProfile } from "./LeetcodeProfile";
 import { getFromPageLocalStorage } from "./util";
 
-let lcProfile: LCProfile;
-
-const initLCProfile = async () => {
+const initLCProfile = async (): Promise<LCProfile | null> => {
   const key = "GLOBAL_DATA:value";
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   const res = await getFromPageLocalStorage(key, tab.id!);
-  if(!res) return;
-  lcProfile = new LCProfile(res);
+  if(!res) return null;
+  return new LCProfile(res);
 }
 
-const getCodeFromLocalStorage = async () => {
-  if(!lcProfile){
-    await initLCProfile();
-  }
+const getCodeFromLocalStorage = async (lcProfile: LCProfile) => {
   console.log("lcProfile", lcProfile);
 }
 
-const messageHandlers = new Map<string, () => Promise<void>>();
+const messageHandlers = new Map<string, (lcProfile: LCProfile) => Promise<void>>();
 messageHandlers.set("getCodeFromLocalStorage", getCodeFromLocalStorage);
 
-chrome.runtime.onMessage.addListener(async (message: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void): Promise<void> => {
-  if(messageHandlers.has(message)){
-    const handler = messageHandlers.get(message)!;
-    await handler();
+(async () => {
+  const lcProfile: LCProfile | null = await initLCProfile();
+  if(!lcProfile) {
+    console.error("lc profile cannot be initialized");
+    return;
   }
-});
+  chrome.runtime.onMessage.addListener(async (message: any, sender: chrome.runtime.MessageSender, sendResponse: (response?: any) => void): Promise<void> => {
+    if(messageHandlers.has(message)){
+      const handler = messageHandlers.get(message)!;
+      await handler(lcProfile);
+    }
+  });
+})()
+
